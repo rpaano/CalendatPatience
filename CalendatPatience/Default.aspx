0ca083e55bceeaf6795137a4ec7f76f8878bf65f
Romel Niño Paano <rpaano02@gmail.com>
2019-09-09 10:50:42 +0800

    disable weekends

---
+++ CalendatPatience/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CalendatPatience.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Calendar Appointment App</h3>
+            <asp:Calendar ID="Calendar1" runat="server" BackColor="White" BorderColor="Black" DayNameFormat="Shortest" Font-Names="Times New Roman" Font-Size="10pt" ForeColor="Black" Height="261px" NextPrevFormat="FullMonth" OnDayRender="Calendar1_DayRender" OnSelectionChanged="Calendar1_SelectionChanged" TitleFormat="Month" Width="438px">
+                <DayHeaderStyle BackColor="#CCCCCC" Font-Bold="True" Font-Size="7pt" ForeColor="#333333" Height="10pt" />
+                <DayStyle Width="14%" />
+                <NextPrevStyle Font-Size="8pt" ForeColor="White" />
+                <OtherMonthDayStyle ForeColor="#999999" />
+                <SelectedDayStyle BackColor="#CC3333" ForeColor="White" />
+                <SelectorStyle BackColor="#CCCCCC" Font-Bold="True" Font-Names="Verdana" Font-Size="8pt" ForeColor="#333333" Width="1%" />
+                <TitleStyle BackColor="Black" Font-Bold="True" Font-Size="13pt" ForeColor="White" Height="14pt" />
+                <TodayDayStyle BackColor="#CCCC99" />
+            </asp:Calendar>
+            <br />
+            <asp:DropDownList ID="lstTimes" runat="server">
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Submit" />
+            <br />
+            <br />
+            <asp:Label ID="lblResult" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CalendatPatience/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalendatPatience
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            lstTimes.Items.Clear();
+
+            switch (Calendar1.SelectedDate.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    lstTimes.Items.Add("7:30 am");
+                    lstTimes.Items.Add("8:30 am");
+                    lstTimes.Items.Add("9:00 am");
+                    break;
+                case DayOfWeek.Saturday:
+                    lstTimes.Items.Add("7:30 am");
+                    break;
+                default:
+                    lstTimes.Items.Add("7:30 am");
+                    lstTimes.Items.Add("8:30 am");
+                    lstTimes.Items.Add("9:00 am");
+                    lstTimes.Items.Add("7:30 pm");
+                    lstTimes.Items.Add("8:30 pm");
+                    lstTimes.Items.Add("9:00 pm");
+                    break;
+            }
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.IsWeekend)
+            {
+                e.Day.IsSelectable = false; 
+            }
+            /*
+            if (e.Day.Date.Day == 15 && e.Day.Date.Month == 3)
+            {
+                e.Cell.BackColor = System.Drawing.Color.Yellow;
+
+                //oop label
+                Label lbl = new Label();
+
+                lbl.Text = "<br/> My Birthday";
+                e.Cell.Controls.Add(lbl);
+            }
+            */
+
+            if (e.Day.Date.Day == 04)
+            {
+                e.Cell.BackColor = System.Drawing.Color.Yellow;
+
+                //oop label
+                Label lbl = new Label();
+
+                lbl.Text = "<br/> Monthsary";
+                e.Cell.Controls.Add(lbl);
+            }
+        }
+    }
+}
